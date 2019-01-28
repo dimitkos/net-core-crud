868274dc62f9c5644f02f887984541976b3bd045
kosmas <kosmas@IBM665-PBF9KY0>
2019-01-28 12:17:56 +0200

    neos fakelos

---
+++ Management.Data/testfile/Classtest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Management.Data.testfile
+{
+    class Classtest2
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string job { get; set; }
+    }
+}
