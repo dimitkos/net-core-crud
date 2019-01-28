feaa5be1462e241e640e7ce6e58404159e9b97fe
kosmas <kosmas@IBM665-PBF9KY0>
2019-01-28 11:35:56 +0200

    first commit

---
+++ Management.Data/EmployeeProvider.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Management.Data
+{
+    public class EmployeeProvider : IEmployeeProvider
+    {
+        private readonly string connectionString;
+
+        public EmployeeProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        
+        public IEnumerable<Employee> Get()
+        {
+            IEnumerable<Employee> employees;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                employees = connection.Query<Employee>("select * from Data");
+            }
+            return employees;
+        }
+    }
+}
