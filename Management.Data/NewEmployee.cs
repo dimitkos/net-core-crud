e22c025207cf6be85bd8392997a1104db38f2759
kosmas <kosmas@IBM665-PBF9KY0>
2019-01-29 15:30:46 +0200

    new klash

---
+++ Management.Data/NewEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Management.Data
+{
+    public class NewEmployee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string job { get; set; }
+    }
+}
